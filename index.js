a169ff56649e81dbcb07e31fb5e8609142b90b09
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-26 09:17:53 -0700

    added random number function

---
+++ index.js
@@ -0,0 +1,7 @@
+function randomNumber1 () {
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * 6;
+    randomNumber = Math.floor(randomNumber) + 1;
+    console.log(randomNumber);
+}
+randomNumber1();
