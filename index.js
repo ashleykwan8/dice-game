e94a27143ae19ba8f7e2682d514df254ce519e88
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-26 10:13:43 -0700

    changed fnumbers randomly of first die

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function randomNumber1 () {
-    let randomNumber = Math.random();
-    randomNumber = randomNumber * 6;
-    randomNumber = Math.floor(randomNumber) + 1;
-    // console.log(randomNumber);
-}
-randomNumber1();
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+let randomImageSource = "images/" + randomDiceImage;
+let dice1 = document.querySelectorAll("img")[0];
+
+dice1.setAttribute("src", randomImageSource);
