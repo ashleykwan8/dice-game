303f36b818b6e8ca6664333b1cb453f6fe686eb8
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-26 10:22:03 -0700

    updated js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomDiceImage = "dice" + randomNumber1 + ".png";
 let randomImageSource = "images/" + randomDiceImage;
-let dice1 = document.querySelectorAll("img")[0];
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
-dice1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let randomDiceImage2 = "dice" + randomNumber2 + ".png";
 let randomImageSource2 = "images/" + randomDiceImage2;
-let dice2 = document.querySelectorAll("img")[1];
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-dice2.setAttribute("src", randomImageSource2);
