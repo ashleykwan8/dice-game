a8c14a42619494945b15b0b9629d726b8a232f76
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-26 10:29:34 -0700

    changed h1 to display winner or draw

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomDiceImage = "dice" + randomNumber1 + ".png";
 let randomImageSource = "images/" + randomDiceImage;
-document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+let dice1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let randomDiceImage2 = "dice" + randomNumber2 + ".png";
 let randomImageSource2 = "images/" + randomDiceImage2;
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+let dice2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "✨ Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "✨ Player 2 Wins!";
+} else {
+    document.querySelector("h1").textContent= "⚔️ Draw!";
+}
