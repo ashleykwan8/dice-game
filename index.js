4160f9eba23c86ebe2aca2fa46656fad01221ee2
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-26 10:15:31 -0700

    both dice change with refresh

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ let randomImageSource = "images/" + randomDiceImage;
 let dice1 = document.querySelectorAll("img")[0];
 
 dice1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage2 = "dice" + randomNumber2 + ".png";
+let randomImageSource2 = "images/" + randomDiceImage2;
+let dice2 = document.querySelectorAll("img")[1];
+
+dice2.setAttribute("src", randomImageSource2);
