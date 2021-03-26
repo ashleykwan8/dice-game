1414a4e178d88ebf7bcb66d1de4d5feb095e360d
Ashley Kwan <ashleykwan888@gmail.com>
2021-03-26 10:34:31 -0700

    changed file name

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ let randomImageSource2 = "images/" + randomDiceImage2;
 let dice2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").textContent = "✨ Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "✨ Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").textContent = "✨ Player 2 Wins!";
+    document.querySelector("h1").innerHTML = "✨ Player 2 Wins!";
 } else {
-    document.querySelector("h1").textContent= "⚔️ Draw!";
+    document.querySelector("h1").innerHTML= "⚔️ Draw!";
 }
